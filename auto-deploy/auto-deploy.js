272ce2117ca8cad1073d67d1dbf3e59cb977b7ec
Xavier Vargas <xtr3m3nerd@gmail.com>
2020-07-29 12:46:30 -0700

    Create auto-deploy script

---
+++ auto-deploy/auto-deploy.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+var express = require("express");
+var app = express();
+var childProcess = require('child_process');
+
+const deployScript = "deploy.sh";
+const port = process.env.PORT ? process.env.PORT : 3000;
+
+app.post("/auto-pull/webhooks/updates-to-master", function(req, res) {
+  childProcess.exec(deployScript, function(err, stdout, stderr){
+    if (err) {
+      console.error(err);
+      return res.sendStatus(500);
+    }
+    console.log(stdout);
+    if(stderr){
+      console.error(stderr);
+    }
+    res.sendStatus(200);
+  });
+});
+
+app.listen(port, () => console.log(`Auto pull listening at http://localhost:${port}`));
