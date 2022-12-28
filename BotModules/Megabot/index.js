c2b43ad56a7490ed49f942483f30db903d069077
Philip R. Beckwith <phi2132160@gmail.com>
2022-12-27 23:37:38 -0800

    Update index.js

--- BotModules/Megabot/index.js
+++ BotModules/Megabot/index.js
@@ -2,7 +2,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 
 const bot = new Client({
 	intents: [
-		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.MessageContent
 	],
 });
 
