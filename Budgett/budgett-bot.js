4fc372d3c8bc7bbc17b0343593d201f4981fbeee
Philip Beckwith <phi2132160@gmail.com>
2023-01-22 22:33:10 -0800

    budgett bot logger update + some time zone fixes

--- Budgett/budgett-bot.js
+++ Budgett/budgett-bot.js
@@ -1,7 +1,10 @@
+const logger = require('../utils/logger')('budgett')
 const schedule = require('node-schedule')
 const { Client, Events, IntentsBitField } = require('discord.js');
 const {getWednesdayMeme} = require('./utils/utils');
 
+const UTC_TO_ARIZONA_TIMEZONE_OFFSET = - 7 * 60 * 60 * 1000;
+const LOCAL_TO_UTC_TIMEZONE_OFFSET = new Date().getTimezoneOffset() * 60 * 1000;
 const chatID = process.env.BUDGETT_CHAT_ID;
 
 let token = process.env.BUDGETT_TOKEN;
@@ -26,47 +29,66 @@ async function sendWednesdayMeme(){
   client.channels.cache.get(chatID).send(await getWednesdayMeme())
 }
 
+
 const logOn = schedule.scheduleJob('0 0 4 * * 3', function(){
-    console.log("It's Wednesday My Dudes!")
-    login();
+  logger.info("It's Wednesday My Dudes!")
+  login();
+});
+
+schedule.scheduleJob({hour: 7, minute: 0, dayOfWeek: 3, tz: 'America/Phoenix' }, function(){
+  logger.info("It's Wednesday My Dudes! (TIMEZONE TEST 7:00 AM America/Phoenix)")
+  login();
 });
 
 const logOut = schedule.scheduleJob('0 30 21 * * 3', function(){
-    console.log("Wednesday's Over My Dudes!")
-    client.destroy();
+  logger.info("Wednesday's Over My Dudes!")
+  client.destroy();
   loggedin = false;
 });
 
 const postWednesdayMessage = schedule.scheduleJob('0 10 9 * * 3', function(){
-    console.log("Reminding the Bois it's wednesday.")
-    if(!loggedin){ login(); }
-    client.channels.cache.get(chatID).send(`It's Wednesday My Dudes!`);
+  logger.info("Reminding the Bois it's wednesday.")
+  if(!loggedin){ login(); }
+  client.channels.cache.get(chatID).send(`It's Wednesday My Dudes!`);
 });
 
 const postWednesdayMeme = schedule.scheduleJob('0 40 11 * * 3', function(){
-    console.log("Meme Time");
-    if(!loggedin){ login(); }
-    sendWednesdayMeme();
+  logger.info("Meme Time");
+  if(!loggedin){ login(); }
+  sendWednesdayMeme();
 });
 
 client.on('ready', () => {
-    console.log('Ready for Wednesday my dude?');
-    if(process.env.ENVIRONMENT == 'STAGING'){
-        console.log("Are you ready kids?")
-        sendWednesdayMeme();
-    }
-    loggedin = true;
+  logger.info('Ready for Wednesday my dude?');
+  if(process.env.ENVIRONMENT == 'STAGING'){
+    logger.info("Are you ready kids?")
+    sendWednesdayMeme();
+  }
+  loggedin = true;
 });
 
 client.on(Events.MessageCreate, message => {
-        if(message.content.match('GIVE ME WEDNESDAY!')){
-          sendWednesdayMeme();
-        }
-  })
+  if(message.content.match('GIVE ME WEDNESDAY!')){
+    sendWednesdayMeme();
+  }
+})
 
-if(process.env.ENVIRONMENT === 'STAGING' || new Date().getDay() === 3){
+function isWednesday(){
+  const date = new Date();
+
+  // Convert to UTC
+  date.setTime(date.getTime() + LOCAL_TO_UTC_TIMEZONE_OFFSET)
+
+  // Convert TO AZ Time
+  date.setTime(date.getTime() + UTC_TO_ARIZONA_TIMEZONE_OFFSET)
+
+  // Check if it's Wednesday.
+  return date.getDay === 3;
+}
+
+if(process.env.ENVIRONMENT === 'STAGING' || isWednesday()){
   login();
-  console.log('It\'s wednesday');
+  logger.info('It\'s wednesday');
 }else{
-  console.log(`Budgett is sleeping for now, he'll return next wednesday.`)
+  logger.info(`Budgett is sleeping for now, he'll return next wednesday.`)
 }
