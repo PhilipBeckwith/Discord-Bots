1dff510fd5ca5d1b18b719872d32f620d3a39343
Philip Beckwith <phi2132160@gmail.com>
2023-01-23 18:42:44 -0800

    Adding support for synchronous and asynchronous functions.

--- utils/newRelic-utils.js
+++ utils/newRelic-utils.js
@@ -1,19 +1,17 @@
 const logger = require('./logger').getLogger('newRelicUtils')
 const newRelic = require('newrelic')
 
+const AsyncFunction = (async()=>{}).constructor
+
 function instrementSegment(method){
     if(method.segmentInstrumented){
         logger.warn(`${method.name} is already an instrumented segment...`)
         return method;
     }
 
-    logger.info(`Instrumented Method ${method.name}`)
-    const instrementedMethod = function(){
-        return newRelic.startSegment(method.name, true, async ()=>{
-            logger.info(`Starting Segment ${method.name}`)
-            return await method(...arguments)
-        })
-    }
+    const instrementedMethod = method instanceof AsyncFunction ? 
+        wrapAsyncSegment(method) :
+        wrapSynchronousSegment(method);
 
     instrementedMethod.segmentInstrumented = true;
     Object.defineProperty(instrementedMethod, 'name', {
@@ -22,6 +20,26 @@ function instrementSegment(method){
       });
 
     return instrementedMethod;
+}
+
+function wrapSynchronousSegment(method){
+    logger.info(`Instrumenting synchronous Segment ${method.name}`)
+    return function(){
+        return newRelic.startSegment(method.name, true, ()=>{
+            logger.info(`Starting Segment ${method.name}`)
+            return method(...arguments)
+        })
+    }
+}
+
+function wrapAsyncSegment(method){
+    logger.info(`Instrumenting Async Segment ${method.name}`)
+    return async function(){
+        return newRelic.startSegment(method.name, true, async ()=>{
+            logger.info(`Starting Segment ${method.name}`)
+            return await method(...arguments)
+        })
+    }
 }
 
 function instrementBackgroundTransaction(method){
@@ -35,14 +53,9 @@ function instrementBackgroundTransaction(method){
     }
 
     logger.info(`Instrumented Method ${method.name}`)
-    const instrementedMethod = function(){
-        return newRelic.startBackgroundTransaction(
-            method.name, async () => {
-                logger.info(`Starting Transaction ${method.name}`)
-                console.time(method.name)
-                return await method(...arguments)
-        })
-    }
+    const instrementedMethod = method instanceof AsyncFunction ? 
+        wrapAsyncTransaction(method) :
+        wrapSynchronousTransaction(method);
 
     instrementedMethod.transactionInstrumented = true;
     Object.defineProperty(instrementedMethod, 'name', {
@@ -51,6 +64,28 @@ function instrementBackgroundTransaction(method){
       });
 
     return instrementedMethod;
+}
+
+function wrapSynchronousTransaction(method){
+    logger.info(`Instrumenting synchronous Transaction ${method.name}`)
+    return function(){
+        return newRelic.startBackgroundTransaction(
+            method.name, () => {
+                logger.info(`Starting Transaction ${method.name}`)
+                return method(...arguments)
+        })
+    }
+}
+
+function wrapAsyncTransaction(method){
+    logger.info(`Instrumenting Async Transaction ${method.name}`)
+    return async function(){
+        return newRelic.startBackgroundTransaction(
+            method.name, async () => {
+                logger.info(`Starting Transaction ${method.name}`)
+                return await method(...arguments)
+        })
+    }
 }
 
 module.exports = {
