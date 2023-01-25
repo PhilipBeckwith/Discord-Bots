bb7e6f41153552bfd6de322482cbdadeadcbc5a9
Philip Beckwith <phi2132160@gmail.com>
2023-01-24 17:56:52 -0800

    blah

--- Mega-Bot/bots/twillio.js
+++ Mega-Bot/bots/twillio.js
@@ -56,6 +56,9 @@ function getUserIds({options}){
   const user = options.getUser('user')
   const members = options.getString('members')
 
+  console.log('HERE')
+  console.log(user)
+  
   if(user)
     return [user.id]
 
